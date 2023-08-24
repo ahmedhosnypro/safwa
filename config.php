edb9e96d83d79bec2525edbc3f7465fbe19a0b13
root <root@ip-172-31-8-72.me-central-1.compute.internal>
2023-08-24 01:32:19 +0000

    Initial commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'examdata';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = 'cgEBv2=X6/25';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'https://ec2-3-28-183-71.me-central-1.compute.amazonaws.com';
+$CFG->dataroot  = '/bitnami/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
